c19d488501dcdc3a6534c3e936d043a00720be0b
Joshua Alphonse <joshalphonse@gmail.com>
2018-12-09 17:55:49 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,5 +1,19 @@
+require 'pry'
 def consolidate_cart(cart)
   # code here
+  result = {}
+  cart.each do |item|
+    result[item_keys] = item.type
+    if result[item_keys][:count].nil?
+      result[item_keys][:count] = 0
+    end
+    result.each do |i|
+      if i.first == item_keys
+        result[item_keys][:count] += 1
+      end
+    end
+  end
+    result
 end
 
 def apply_coupons(cart, coupons)
